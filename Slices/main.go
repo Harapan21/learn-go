b200f6551f05fd357c4742319fb1c7b6f5c785da
harapan <harapanpardamean@gmail.com>
2019-06-19 07:59:56 +0700

    .

--- Slices/main.go
+++ Slices/main.go
@@ -1,17 +1,49 @@
 package main
 
-import "fmt" 
+import "fmt"
+
 func main() {
 	// slice lebih powefull dari array
 	// slice tidak sama seperti array
-	// setiap element yang ada di slice harus 1 tipe data
 	// sesuai nama slice dapat memotong string utuh menjadi char
 	s := make([]string, 3)
 	fmt.Println("emp", s)
 
-
+	// sama seperti untuk mengubah nilai
 	s[0] = "a"
 	s[1] = "b"
 	s[2] = "c"
 	fmt.Println(s)
+	fmt.Println(s[2])
+	fmt.Println("len", len(s))
+
+	// menambahkan nilai
+	s = append(s, "d")
+	s = append(s, "e", "f")
+	fmt.Println("apd:", s)
+
+	// copy value s ke c
+	c := make([]string, len(s))
+	copy(c, s)
+	fmt.Println(c)
+
+	// mengambil nilai s
+	// dari index array 2
+	// sampai index arrray 5 - 1 = 4
+	l := s[2:5]
+	fmt.Println(l)
+
+	// memotong sampai ( tidak termasuk ) s[5]
+	l = s[:5]
+	fmt.Println(l)
+
+	// memotong mulai dari ( termasuk  ) s[2]
+	l = s[2:]
+	fmt.Println(l)
+	// deklarasi slice secara langsung
+	t := []string{"g", "h", "t"}
+	fmt.Println(t)
+
+
+	
 }
--- Variables/main.go
+++ Variables/main.go
@@ -3,29 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	// menggunakan var teknik pertama
-	var a = "this is var"
-	fmt.Println(a)
+	x := 2
+	perulangan(x)
+}
 
-	// menggunakan var teknik kedua
-	// var sekaligus deklarasi dua, dan type datanya
-	var v, c int = 1, 2
-	fmt.Println(v, c)
+func nambah(x int) int {
+	return x + 1
+}
 
-	// deklarasi var teknik ketiga
-	var d = true
-	fmt.Println(d)
-
-	// deklarasi var teknik keempat
-	// var dengan tipe tipe data integer, dengan nilai kosong
-	// otomatis akan ke nilai 0
-	var e int
-	fmt.Println(e)
-
-	// deklarasi variable dengan cara cepat tanpa menggunakan
-	// tipe data
-	// contoh dibawah sama saja dengan
-	// var f string = "cara cepat"
-	f := "cara cepat"
-	fmt.Println(f)
+func perulangan(i int) {
+	for i <= 3 {
+		fmt.Println(i)
+		i = i + 1
+	}
 }
